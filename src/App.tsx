fb4374115a17ff78790810d720d52aede147cf7e
Igor Lourenço <theigorlourenco@gmail.com>
2020-10-13 11:53:46 -0300

    Initial layout settings

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 
+import './styles/global.css'
+
 function App() {
     return (
         <div className="App">
-            <h1>Happy</h1>
+        <h1>Happy</h1>
         </div>
     );
 }
