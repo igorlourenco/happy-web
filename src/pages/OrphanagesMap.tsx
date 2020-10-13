3f584a2f7ea922e947a57ec79b2fea416efa7d6c
Igor Lourenço <theigorlourenco@gmail.com>
2020-10-13 15:06:51 -0300

    Create Orphanages Map page

--- src/pages/OrphanagesMap.tsx
+++ src/pages/OrphanagesMap.tsx
@@ -1,9 +1,33 @@
 import React from "react";
+import {Link} from 'react-router-dom';
+import mapMarkerImg from '../images/map-marker.svg';
+import {FiPlus} from "react-icons/all";
 
+import '../styles/pages/orphanages-map.css';
 
 function OrphanagesMap (){
     return (
-        <h1>OrphanagesMap</h1>
+        <div id="page-map">
+            <aside>
+                <header>
+                    <img src={mapMarkerImg} alt="Happy"/>
+
+                    <h2>Escolha um orfanato no mapa</h2>
+                    <p>Muitas crianças estão esperando a sua visita :)</p>
+                </header>
+
+                <footer>
+                    <strong>Palmas</strong>
+                    <span>Tocantins</span>
+                </footer>
+            </aside>
+
+            <div></div>
+
+            <Link to="" className="create-orphanage">
+                <FiPlus size={32} color="#FFF"/>
+            </Link>
+        </div>
     );
 }
 
